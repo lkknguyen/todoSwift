13d3b4fbd1ad0bbc91fc4914213300c0c8f54259
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2017-09-19 22:04:57 +0200

    Rename 'ILAppDelegate' to 'AppDelegate'

--- SingleTodoObjectiveC/SingleTodo/AppDelegate.m
+++ SingleTodoObjectiveC/SingleTodo/AppDelegate.m
@@ -6,10 +6,10 @@
 //  Copyright (c) 2014 icalialabs. All rights reserved.
 //
 
-#import "ILAppDelegate.h"
+#import "AppDelegate.h"
 #import "ILTodoTableViewController.h"
 
-@implementation ILAppDelegate
+@implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
